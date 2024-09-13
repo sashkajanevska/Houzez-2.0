350f58749bdbca4975568c392c90f1ca7a86acf7
sashkajanevska <janevska_sashka@yahoo.com>
2024-09-13 11:32:26 +0200

    desktop header done

---
+++ houzez-2.0/src/components/header/PagesMenu.jsx
@@ -0,0 +1,239 @@
+const PagesMenu = () => {
+  return (
+    <ul
+      class="dropdown-content menu p-0 shadow bg-base-100 w-[250px]"
+      style={{ transform: "scale(1)" }}
+    >
+      <li class="relative group">
+        <a href="">
+          AGENT
+          <div className="svg-wrapper">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18px"
+              height="13px"
+              viewBox="0 0 300 1024"
+            >
+              <path
+                fill="#5b6d7a"
+                d="M338.752 104.704a64 64 0 0 0 0 90.496l316.8 316.8-316.8 316.8a64 64 0 0 0 90.496 90.496l362.048-362.048a64 64 0 0 0 0-90.496L429.248 104.704a64 64 0 0 0-90.496 0z"
+              />
+            </svg>
+          </div>
+        </a>
+        <ul class="absolute left-full top-0 hidden group-hover:block p-0 m-0 bg-base-100 shadow-lg w-[250px]">
+          <li>
+            <a href="">ALL AGENTS V1</a>
+          </li>
+          <li>
+            <a href="">ALL AGENTS V2</a>
+          </li>
+          <li>
+            <a href="">ALL AGENTS V3</a>
+          </li>
+          <li>
+            <a href="">AGENT PROFILE V1</a>
+          </li>
+          <li class="border-b-0">
+            <a href="">AGENT PROFILE V2</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="relative group">
+        <a href="">
+          AGENCIES
+          <div className="svg-wrapper">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18px"
+              height="13px"
+              viewBox="0 0 300 1024"
+            >
+              <path
+                fill="#5b6d7a"
+                d="M338.752 104.704a64 64 0 0 0 0 90.496l316.8 316.8-316.8 316.8a64 64 0 0 0 90.496 90.496l362.048-362.048a64 64 0 0 0 0-90.496L429.248 104.704a64 64 0 0 0-90.496 0z"
+              />
+            </svg>
+          </div>
+        </a>
+        <ul class="absolute left-full top-0 hidden group-hover:block p-0 m-0 bg-base-100 shadow-lg w-[250px]">
+          <li>
+            <a href="">ALL AGENCIES V1</a>
+          </li>
+          <li>
+            <a href="">ALL AGENCIES V2</a>
+          </li>
+          <li>
+            <a href="">AGENCY PROFILE V1</a>
+          </li>
+          <li class="border-b-0">
+            <a href="">AGENCY PROFILE V2</a>
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="" className="new-feature">
+          CONTACT
+        </a>
+      </li>
+
+      <li>
+        <a href="" className="new-feature">
+          INQUIRY FORM
+        </a>
+      </li>
+
+      <li>
+        <a href="" className="new-feature">
+          ABOUT US
+        </a>
+      </li>
+
+      <li class="relative group">
+        <a href="">
+          PROPERTY STATUS
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18px"
+            height="13px"
+            viewBox="0 0 300 1024"
+          >
+            <path
+              fill="#5b6d7a"
+              d="M338.752 104.704a64 64 0 0 0 0 90.496l316.8 316.8-316.8 316.8a64 64 0 0 0 90.496 90.496l362.048-362.048a64 64 0 0 0 0-90.496L429.248 104.704a64 64 0 0 0-90.496 0z"
+            />
+          </svg>
+        </a>
+        <ul class="absolute left-full top-0 hidden group-hover:block p-0 m-0 bg-base-100 shadow-lg w-[250px]">
+          <li>
+            <a href="">FOR RENT</a>
+          </li>
+          <li class="border-b-0">
+            <a href="">FOR SALE</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="relative group">
+        <a href="">
+          PROPERTY TYPE
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18px"
+            height="13px"
+            viewBox="0 0 300 1024"
+          >
+            <path
+              fill="#5b6d7a"
+              d="M338.752 104.704a64 64 0 0 0 0 90.496l316.8 316.8-316.8 316.8a64 64 0 0 0 90.496 90.496l362.048-362.048a64 64 0 0 0 0-90.496L429.248 104.704a64 64 0 0 0-90.496 0z"
+            />
+          </svg>
+        </a>
+        <ul class="absolute left-full top-0 hidden group-hover:block p-0 m-0 bg-base-100 shadow-lg w-[250px]">
+          <li>
+            <a href="">APARTMENT</a>
+          </li>
+          <li>
+            <a href="">SINGLE FAMILY HOME</a>
+          </li>
+          <li class="border-b-0">
+            <a href="">VILLA</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="relative group">
+        <a href="">
+          PROPERTY CITY
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18px"
+            height="13px"
+            viewBox="0 0 300 1024"
+          >
+            <path
+              fill="#5b6d7a"
+              d="M338.752 104.704a64 64 0 0 0 0 90.496l316.8 316.8-316.8 316.8a64 64 0 0 0 90.496 90.496l362.048-362.048a64 64 0 0 0 0-90.496L429.248 104.704a64 64 0 0 0-90.496 0z"
+            />
+          </svg>
+        </a>
+        <ul class="absolute left-full top-0 hidden group-hover:block p-0 m-0 bg-base-100 shadow-lg w-[250px]">
+          <li>
+            <a href="">MIAMI</a>
+          </li>
+          <li class="border-b-0">
+            <a href="">LOS ANGELES</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="relative group">
+        <a href="">
+          PROPERTY FEATURES
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18px"
+            height="13px"
+            viewBox="0 0 300 1024"
+          >
+            <path
+              fill="#5b6d7a"
+              d="M338.752 104.704a64 64 0 0 0 0 90.496l316.8 316.8-316.8 316.8a64 64 0 0 0 90.496 90.496l362.048-362.048a64 64 0 0 0 0-90.496L429.248 104.704a64 64 0 0 0-90.496 0z"
+            />
+          </svg>
+        </a>
+        <ul class="absolute left-full top-0 hidden group-hover:block p-0 m-0 bg-base-100 shadow-lg w-[250px]">
+          <li>
+            <a href="">SWIMMING POOL</a>
+          </li>
+          <li>
+            <a href="">WIFI</a>
+          </li>
+          <li>
+            <a href="">GYM</a>
+          </li>
+          <li class="border-b-0">
+            <a href="">LAUNDRY</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="relative group">
+        <a href="">
+          BLOG
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18px"
+            height="13px"
+            viewBox="0 0 300 1024"
+          >
+            <path
+              fill="#5b6d7a"
+              d="M338.752 104.704a64 64 0 0 0 0 90.496l316.8 316.8-316.8 316.8a64 64 0 0 0 90.496 90.496l362.048-362.048a64 64 0 0 0 0-90.496L429.248 104.704a64 64 0 0 0-90.496 0z"
+            />
+          </svg>
+        </a>
+        <ul class="absolute left-full top-0 hidden group-hover:block p-0 m-0 bg-base-100 shadow-lg w-[250px]">
+          <li>
+            <a href="">SIMPLE BLOG</a>
+          </li>
+          <li class="border-b-0">
+            <a href="">MASONRY BLOG</a>
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="">TYPOGRAPHY</a>
+      </li>
+
+      <li class="border-b-0">
+        <a href="">404 PAGE</a>
+      </li>
+    </ul>
+  );
+};
+
+export default PagesMenu;
